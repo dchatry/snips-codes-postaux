44e377c0236472e0b7d151181c9502bc0ab1c25d
Guillerm David <dguillerm@gmail.com>
2018-10-06 17:27:08 +0200

    split functions

--- action-CodesPostaux.py
+++ action-CodesPostaux.py
@@ -30,37 +30,34 @@ def subscribe_intent_callback(hermes, intentMessage):
     action_wrapper(hermes, intentMessage, conf)
 
 
-def action_wrapper(hermes, intentMessage, conf):
-    """ Write the body of the function that will be executed once the intent is recognized. 
-    In your scope, you have the following objects : 
-    - intentMessage : an object that represents the recognized intent
-    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined. 
-      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName'] 
-     
-    Refer to the documentation for further details. 
-    """
-    ville = intentMessage.slots.ville.first().value
-    print "Intent {}".format(intentMessage.intent)
+def call_http_opendata(curville):
     response = requests.get("https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q="+ville+"&facet=insee_com&facet=nom_dept&facet=nom_region&facet=statut")
     json_data = simplejson.loads(response.text)
-    if (intentMessage.intent.intent_name == "codepostal"):
-        codepostal = json_data['records'][0]['fields']['postal_code'] 
-        result_sentence ="Le code postal de {} est {}.".format(ville,str(codepostal))
-    if (intentMessage.intent.intent_name == "departement"):
-        departement = json_data['records'][0]['fields']['nom_dept'] 
-        numdep = json_data['records'][0]['fields']['code_dept'] 
-        result_sentence ="Le département de {} est {} avec le numéro {}.".format(ville,str(departement),str(numdep))
+    return json_data
 
-    
+def process_codepostal(hermes, intentMessage):
+    ville = intentMessage.slots.ville.first().value
+    jsdata = call_http_opendata(ville)
+    codepostal = json_data['records'][0]['fields']['postal_code'] 
+    result_sentence ="Le code postal de {} est {}.".format(ville,str(codepostal))
+    snips_speak(hermes, intentMessage,result_sentence)
+
+def process_departement(hermes, intentMessage):
+    ville = intentMessage.slots.ville.first().value
+    jsdata = call_http_opendata(ville)
+    departement = json_data['records'][0]['fields']['nom_dept'] 
+    numdep = json_data['records'][0]['fields']['code_dept'] 
+    result_sentence ="Le département de {} est {} avec le numéro {}.".format(ville,str(departement),str(numdep))
+    snips_speak(hermes, intentMessage,result_sentence)
+
+def snips_speak(hermes, intentMessage,sentence):
     current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
-    
+    hermes.publish_end_session(current_session_id, sentence)
 
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("roozeec:codepostal", subscribe_intent_callback) \
-         .subscribe_intent("roozeec:departement", subscribe_intent_callback) \
+        h.subscribe_intent("roozeec:codepostal", process_codepostal) \
+         .subscribe_intent("roozeec:departement", process_departement) \
          .start()
 
