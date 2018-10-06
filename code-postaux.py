43a2897a49227bc4b1003f4facc775febba1d49d
Guillerm David <dguillerm@gmail.com>
2018-10-06 15:32:37 +0200

    fix errors

--- code-postaux.py
+++ code-postaux.py
@@ -1,40 +1,61 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import ConfigParser
 from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
 import simplejson
 import requests
 
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
 
-def intent_received(hermes, intentMessage):
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def action_wrapper(hermes, intentMessage, conf):
     """ Write the body of the function that will be executed once the intent is recognized. 
     In your scope, you have the following objects : 
     - intentMessage : an object that represents the recognized intent
     - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined 
-
+    - conf : a dictionary that holds the skills parameters you defined. 
+      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName'] 
+     
     Refer to the documentation for further details. 
     """ 
+    #ville = "Brest"
+    ville = intentMessage.slots.ville.first().value
+    #result_sentence="Ville {}".format(str(ville))
+    response = requests.get("https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q="+ville+"&facet=insee_com&facet=nom_dept&facet=nom_region&facet=statut")
+    json_data = simplejson.loads(response.text)
+    codepostal = json_data['records'][0]['fields']['postal_code']
 
-    # if len(intentMessage.slots.ville) > 0:
-    #     ville = int(intentMessage.slots.ville.first().value)
-    #     response = requests.get("https://public.opendatasoft.com/api/records/1.0/search/?dataset=correspondance-code-insee-code-postal&q="+ville+"&facet=insee_com&facet=nom_dept&facet=nom_region&facet=statut")
-    #     json_data = simplejson.loads(response.text)
-    #     codepostal = json_data['records'][0]['fields']['postal_code']
-
-        # result_sentence = "Le code postal de {} est {} .".format(ville, codepostal)
-    #     result_sentence = ville
-    # else:
-        # result_sentence = "je ne te connais pas"
-    if intentMessage.intent.intent_name == 'codepostal':
-        print "ok intent code postal"
-        result_sentence = "test"
-    else:
-        return
+    result_sentence = "Le code postal de {} est {} .".format(str(ville), str(codepostal))
 
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
+    
 
 
-with Hermes("localhost:1883") as h:
-    h.subscribe_intents(intent_received).start()        
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("roozeec:codepostal", subscribe_intent_callback) \
+         .start()
+
